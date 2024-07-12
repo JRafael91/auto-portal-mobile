0d8d36a3cd1666f71bf7d7c04bef25a43c4af04e
Rafael Acosta <rafael@tecnofy.org>
2024-07-11 20:02:42 -0700

    feat: rutas de acceso y protección de rutas #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
+import { authGuard } from './utils/auth.guard'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { 
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { 
+    path: 'auth',
+    loadChildren: () => import('~/app/auth/auth.module').then((m) => m.AuthModule)
+  },
   {
     path: 'home',
     loadChildren: () => import('~/app/home/home.module').then((m) => m.HomeModule),
+    canActivate: [authGuard]
   },
   {
     path: 'browse',
